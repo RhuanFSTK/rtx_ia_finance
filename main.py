ed4dc03eb82eee23b4001f2e3f97feaa76785efd
Rhuan <rhuanfullstack@gmail.com>
2025-05-21 02:39:31 -0300

    correção main

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from endpoints import registro, transcricao, imagem
 
 app = FastAPI(title="Agente Financeiro com IA")
 
-app.include_router(registro.router, prefix="/registro")
-app.include_router(transcricao.router, prefix="/audio")
-app.include_router(imagem.router, prefix="/imagem")
+# app.include_router(registro.router, prefix="/registro")
+# app.include_router(transcricao.router, prefix="/audio")
+# app.include_router(imagem.router, prefix="/imagem")
 
 @app.get("/")
 def read_root():
