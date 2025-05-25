3e5a0430fb63b8846b5741a44f4b1c4c7e3dec0c
Rhuan <rhuanfullstack@gmail.com>
2025-05-25 12:33:11 -0300

    data_hora

--- endpoints/registro.py
+++ endpoints/registro.py
@@ -34,11 +34,13 @@ def registrar_gasto(descricao: str = Form(...)):
         # Pega data e hora de Brasília
         fuso_brasilia = pytz.timezone("America/Sao_Paulo")
         data_hora_brasilia = datetime.now(fuso_brasilia).strftime("%Y-%m-%d %H:%M:%S")
+        
+        print(data_hora_brasilia)
 
         with get_connection() as conn:
             cursor = conn.cursor()
             cursor.execute(
-                "INSERT INTO gastos (descricao, classificacao, valor, data_hora) VALUES (%s, %s, %s, %s)",
+                "INSERT INTO gastos (descricao, classificacao, valor, 'data_hora') VALUES (%s, %s, %s, %s)",
                 (desc, classificacao, valor, data_hora_brasilia)
             )
             conn.commit()
